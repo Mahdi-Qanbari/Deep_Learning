49e4751a8cfe4e59df3521228e6fd0bdc2bed21d
Mahdi Qanbari <55098241+Mahdi-Qanbari@users.noreply.github.com>
2024-12-07 11:03:40 +0100

    Delete exercise2_material/src_to_implement/Layers/Base.py

--- exercise2_material/src_to_implement/Layers/Base.py
+++
@@ -1,9 +0,0 @@
-class BaseLayer(object):
-    def __init__(self):
-        self.trainable = False
-        self.weights = []
-    
-    def forward(self):
-        raise NotImplementedError
-    
-    
